4cb7a991d2b78eea211eef0620ba86e6a0faf1c8
annchana <annchanaoat@gmail.com>
2024-12-06 01:18:15 +0700

    database

--- social/cmd/api/main.go
+++ social/cmd/api/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"log"
 
+	"github.com/Unchana19/social/internal/db"
 	"github.com/Unchana19/social/internal/env"
+	"github.com/Unchana19/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":9000"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://postgres:password@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Printf("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
--- social/go.mod
+++ social/go.mod
@@ -2,4 +2,7 @@ module github.com/Unchana19/social
 
 go 1.23.0
 
-require github.com/go-chi/chi/v5 v5.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/lib/pq v1.10.9
+)
