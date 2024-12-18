815ae04a8994ac0b97ae429d40c981afff805d93
annchana <annchanaoat@gmail.com>
2024-12-19 00:05:47 +0700

    logger

--- social/cmd/api/main.go
+++ social/cmd/api/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Unchana19/social/internal/db"
 	"github.com/Unchana19/social/internal/env"
 	"github.com/Unchana19/social/internal/store"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.1"
@@ -40,21 +39,27 @@ func main() {
 		env: env.GetString("ENV", "delvelopment"),
 	}
 
+	// Logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// Database
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
 	if err != nil {
-		log.Panic(err)
+		logger.Fatal(err)
 	}
 
 	defer db.Close()
-	log.Printf("database connection pool established")
+	logger.Info("database connection pool established")
 
 	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
 		store:  store,
+		logger: logger,
 	}
 
 	mux := app.mount()
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 }
