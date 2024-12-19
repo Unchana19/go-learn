2cbb78a8ea00a3c6579c44b76c2f93fa9108c094
annchana <annchanaoat@gmail.com>
2024-12-20 00:09:32 +0700

    authenticate

--- social/cmd/api/api.go
+++ social/cmd/api/api.go
@@ -74,7 +74,11 @@ func (app *application) mount() http.Handler {
 				r.Get("/feed", app.getUserFeedHandler)
 			})
 		})
-
+		
+		// Public routes
+		r.Route("/authentication", func(r chi.Router) {
+			r.Post("/user", app.registerUserHandler)
+		})
 	})
 
 	return r
